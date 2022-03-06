82aa7979f1c2e094461f4308904de803e3ecb493
Sabarinathan N <sabarinathan25052001@gmail.com>
2022-03-06 20:02:35 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//step-1 create an xmlhttp resquest
+var request=new XMLHttpRequest();
+// step-2 choose the data (ex:API url choosing)
+request.open("GET","https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json");
+//step-3 creating path for client to server
+request.send();
+//step-4 string format into json format for redability
+request.onload=function(){
+    var data=JSON.parse(request.response);
+    console.log(data);
+var res=data.filter((ele)=>ele.region==="asia");
+for(var i=0;i<res.length;i++){
+    console.log(res[i].name)
+}
+}
